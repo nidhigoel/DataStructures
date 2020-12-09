cba886c6d30cc94bd0495af7bf11ed4f28278fc6
nidhigoel <nidhigoel95@gmail.com>
2020-12-09 15:00:57 +0530

    Create Intersection.java
    TODO add code

---
+++ LinkedLists/Intersection.java
@@ -0,0 +1,3 @@
+// Find intersection of two linked list
+// find difference between length of linked list, advance longer linked list by diff d and then advance both pointers by one element until next of both pointer is eaual
+// other approaches - brute force, hash table, sorting on node addresses and searching
