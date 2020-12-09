d00a7c3db4fb7c9bd0011b743d54596e8d991547
nidhigoel <nidhigoel95@gmail.com>
2020-12-09 17:57:39 +0530

    Add code to reverse in pairs and in blocks

--- LinkedLists/Reverse.java
+++ LinkedLists/Reverse.java
@@ -22,9 +22,13 @@ class LinkedList {
 		ll.insert(3);
 		ll.insert(4);
 		ll.print();
-		ll.reverserec(ll.head);
-		ll.print();
-		ll.reverse();
+		// ll.reverserec(ll.head);
+		// ll.print();
+		// ll.reverse();
+		// ll.print();
+		// ll.reverseInBlocks(3);
+		// ll.reverseInPairs();
+		ll.head = ll.reverseInPairsRec(ll.head);
 		ll.print();
 	}
 	
@@ -75,5 +79,67 @@ class LinkedList {
 		head = reverse;
 		return reverse;
 		
+	}
+	
+	public Node reverseInPairsRec(Node head){
+		if(head==null||head.next==null)return head;
+		Node temp = head;
+		Node temp1=head.next;
+		Node temp2 = head.next.next;
+		
+		temp1.next=temp;
+		temp.next=reverseInPairsRec(temp2);
+		return temp1;
+	}
+	
+	public void reverseInPairs(){
+		// if(head==null || head.next==null) return;
+		Node temp=head;
+		Node prev_last =null, cur_last=null;
+		Boolean flag=true;
+		while(temp!=null && temp.next!=null){
+			prev_last=cur_last;
+			cur_last = temp;
+			Node next = temp.next;
+			Node next2 = temp.next.next;
+			next.next=temp;
+			temp.next=null;
+			if(flag){
+			head = next;
+			flag=false;}
+			temp = next2;
+			if(prev_last!=null){
+				prev_last.next=next;
+			}
+			
+		}
+		if(temp!=null){
+			prev_last.next=temp;
+		}
+	}
+	
+	public void reverseInBlocks(int blockSize){
+
+		Node temp = head;
+		boolean flag = true;
+		Node prev_head=null, prev_last=null, cur_head=null, cur_last = null;
+		while(temp!=null){
+			Node temp1 = temp;
+			Node prev = null;
+			for(int i=0; temp!=null && i<blockSize; i++){
+				Node next = temp.next;
+				temp.next = prev;
+				prev = temp;
+				temp = next;
+			}
+			if(flag){
+			head = prev; flag=false;}
+			prev_head=cur_head;
+			prev_last=cur_last;
+			cur_head = prev;
+			cur_last = temp1;
+			if(prev_last!=null){
+			prev_last.next = cur_head;}
+		}
 	}
 }
